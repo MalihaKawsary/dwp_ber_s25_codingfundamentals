3698a0883b99dcf3a8da5b69651f0a34d30b35fd
MalihaKawsary <malihakawsaryipe12@gmail.com>
2025-03-19 10:24:24 +0000

    main.py homework/lesson-2

---
+++ homework/lesson-2/main.py
@@ -0,0 +1,72 @@
+# Variables and Basic Data types
+# my_number=10
+# my_string="Hello,Python"
+# my_float=3.14
+# print(my_number)
+# print(my_string)
+# print(my_float)
+# working with different data types
+# a. string concatenation
+# first_name="Maliha"
+# last_name="Kawsary"
+# full_name= first_name + " "+ last_name
+# print(full_name)
+# b. Arithmetic operations
+# a=5
+# b=3
+# add_result=a+b
+# sub_result=a-b
+# mult_result=a*b
+# div_result=a/b
+# print(add_result)
+# print(sub_result)
+# print(mult_result)
+# print(div_result)
+# booleans and comparisons
+# a.creating booleans
+# is_greater=5>3
+# is_equal=5==3
+# is_smaller=5<3
+# print(is_greater)
+# print(is_equal)
+# print(is_smaller)
+# b. booleans operations
+bool1= True
+bool2= False
+# and_logical=bool1*bool2
+# print(and_logical)
+# or_logical=bool1+bool2
+# print(or_logical)
+if not bool1:print(bool2)
+# c.comparison between data types
+# pi=3.14
+# pi_pi='3.14'
+# pi_pi_pi="3.14"
+# result_1=pi==pi_pi
+# print(result_1)
+# cooment: pi is number and pi_pi is string
+# result_2=pi_pi==pi_pi_pi
+# print(result_2)
+# Type checking and conversion
+# a.type checking
+# pi=3.14
+# pi_pi='3.14'
+# pi_pi_pi="3.14"
+# type(pi)
+# type(pi_pi)
+# type(pi_pi_pi)
+# print(type(pi))
+# print(type(pi_pi))
+# print(type(pi_pi_pi))
+# b.type conversion
+# my_str="123"
+# my_int= int(my_str)
+# my_float_converted= float(my_int)
+# print(my_str)
+# print(my_int)
+# print(my_float_converted)
+# 5.Challenge
+# celsius= 12
+# fahrenheit=(celsius*9/5)+32
+# print(celsius)
+# print(fahrenheit)
